125b8bbe9776fe61d8a619f2d7438f7e750ae712
Rr210 <1577791638@qq.com>
2021-08-21 22:20:32 +0800

    feat:部署vercel

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
 # -*- coding: UTF-8 -*-
-import re, requests,json
+import re
+import requests
+import json
 from http.server import BaseHTTPRequestHandler
 
 '''
 微博热搜爬取
 '''
-hot_all_databae = []
 
 
 class WBO:
@@ -16,14 +17,20 @@ class WBO:
         res = requests.get(self.url)
         res.encoding = 'utf8'
         hot_title = re.findall('<tr class="">(.*?)</tr>', res.text, re.S)
+        hot_all_databae = []
         for index, item in enumerate(hot_title):
-            hot_T = re.search('<td class="td-02">(?P<Cnode>.*?)</td>', item, re.S)
-            title = re.search('<a href(|_to)="(?P<link>.*?)".*?(?P<tit>.*?)</a>', hot_T.group('Cnode'), re.S)
-            hot_I = re.search('<td class="td-03"><i class="icon-txt icon-txt-\w+">(?P<hotI>.*?)</i></td>', item, re.S)
-            hot_n = re.search('<span>(?P<CN>.*?)</span>', hot_T.group('Cnode'), re.S)
+            hot_T = re.search(
+                '<td class="td-02">(?P<Cnode>.*?)</td>', item, re.S)
+            title = re.search(
+                '<a href(|_to)="(?P<link>.*?)".*?(?P<tit>.*?)</a>', hot_T.group('Cnode'), re.S)
+            hot_I = re.search(
+                '<td class="td-03"><i class="icon-txt icon-txt-\w+">(?P<hotI>.*?)</i></td>', item, re.S)
+            hot_n = re.search('<span>(?P<CN>.*?)</span>',
+                              hot_T.group('Cnode'), re.S)
             hot_N = '' if not hot_n else hot_n.group('CN')
             hot_status = '' if not hot_I else hot_I.group('hotI')
             res_obj = {
+                "id": index+1,
                 "title": title.group('tit'),
                 "link": title.group('link'),
                 "hot_count": hot_N,
@@ -33,20 +40,18 @@ class WBO:
         return hot_all_databae
 
 
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        data= []
+        data = []
         path = self.path
         user = path.split('?tit=')[1]
-        if user=='wb':
+        if user == 'wb':
             data = WBO().reqs()
         else:
-            data.append({"message":"参数有误","code":"-1"})
+            data.append({"message": "参数有误", "code": "-1"})
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
         return
-
